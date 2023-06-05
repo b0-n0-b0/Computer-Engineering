d4000c7bc76b85bf589a81c153b5972c1f6fdae4
Fabio <fabizen99@gmail.com>
2023-06-05 18:26:51 +0200

    add 05/06/23 exam with answers (some of them)

---
+++ FIRST_YEAR/SECOND_SEMESTER/Computer_Architecture/Quantum/05-06-23/Question.md
@@ -0,0 +1,7 @@
+1. Calculate the various stage (barrier in the circuit) Y1, Y2, Y3, Y4, Y5
+2. Calculate the probability of the mesuraments 
+    Version_A P(00)=P(01)=P(10)=P(11)=1/4
+    Version_B P(00)=P(11)=1/2
+3. Calculate the quantum vector after the mesurament of |11>
+    [0 0 0 1] (is a column vector so translate it)
+4. Calculate the matrix U given by the union of Rx and S (do the tensor product not the matrix multiplicantion !!!!!!)
---
+++ FIRST_YEAR/SECOND_SEMESTER/Computer_Architecture/Quantum/05-06-23/Version_B/Code.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+from qiskit import IBMQ, Aer
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, execute
+from qiskit.visualization import plot_histogram, plot_bloch_multivector
+from math import pi
+#Register creation
+qr = QuantumRegister(2) #Quantum reg
+cr = ClassicalRegister(2) #classic reg
+
+exam = QuantumCircuit(qr, cr) #Circuit creation
+
+exam.barrier() #Y1
+
+# H and X gates
+exam.x(qr[1])
+exam.h(qr[0])
+exam.barrier() #Y2
+
+#CNot gate
+exam.cx(qr[0], qr[1])
+exam.barrier() #Y3
+
+#Rx(pi/2) gate
+exam.rx(pi,qr[1])
+exam.barrier() #Y4
+
+#S gate
+exam.s(qr[0])
+exam.barrier() #Y5
+
+exam.measure(qr[0], cr[1])
+exam.measure(qr[1], cr[0])
+
+exam.draw(output='mpl')
+
+
+shots = 2048
+
+backend = Aer.get_backend('qasm_simulator') #get the backend
+res_exe = execute(exam, backend=backend, shots=shots).result() #calculate the result
+res = res_exe.get_counts() 
+#print and plot the result
+print(res)
+plot_histogram(res)
